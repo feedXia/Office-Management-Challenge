284b1467cf72102b7bf7af37ac813aa00511bd2f
Xia <zeyaoliu@me.com>
2021-10-29 13:50:51 +0100

    feat: add addRoom & listRooms methods with passing tests

---
+++ app/src/test/java/officeManagement/MeetingRoomTest.java
@@ -0,0 +1,30 @@
+package officeManagement;
+
+import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
+// import static org.junit.Assert.assertTrue;
+// import static org.junit.Assert.assertFalse;
+
+// import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+public class MeetingRoomTest {
+  @Test
+  public void testAddRoom() {
+    MeetingRoom room = new MeetingRoom();
+    assertEquals(room.addRoom("Shukaku"), "Shukaku");
+  }
+
+  @Test
+  public void testListRooms() {
+    MeetingRoom room = new MeetingRoom();
+    room.addRoom("Shukaku");
+    room.addRoom("Matatabi");
+    room.addRoom("Isobu");
+    assertTrue(room.listRooms().contains("Shukaku"));
+    assertTrue(room.listRooms().contains("Matatabi"));
+    assertTrue(room.listRooms().contains("Isobu"));
+  }
+}
